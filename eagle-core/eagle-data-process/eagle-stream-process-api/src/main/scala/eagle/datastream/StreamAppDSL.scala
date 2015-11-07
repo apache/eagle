4b76b8fb5319410c622458884b27fc9c8486f93a
Hao Chen <hchen9@ebay.com>
2015-11-07 19:13:04 +0800

    [EAGLE-5] Expand StreamAppDSL get return types

--- eagle-core/eagle-data-process/eagle-stream-process-api/src/main/scala/eagle/datastream/StreamAppDSL.scala
+++ eagle-core/eagle-data-process/eagle-stream-process-api/src/main/scala/eagle/datastream/StreamAppDSL.scala
@@ -1,7 +1,7 @@
 package eagle.datastream
 
 import backtype.storm.topology.base.BaseRichSpout
-import com.typesafe.config.{ConfigValueFactory, Config}
+import com.typesafe.config.{ConfigValue, ConfigObject, ConfigValueFactory, Config}
 import eagle.dataproc.impl.storm.AbstractStormSpoutProvider
 import eagle.dataproc.util.ConfigOptionParser
 
@@ -33,10 +33,32 @@ trait ConfigContext{
 
   def get[T](key:String)(implicit tag: TypeTag[T]):T = tag.tpe match {
     case STRING_TYPE => config.getString(key).asInstanceOf[T]
+    case TypeTag.Double => config.getDouble(key).asInstanceOf[T]
+    case TypeTag.Long => config.getLong(key).asInstanceOf[T]
+    case TypeTag.Int => config.getInt(key).asInstanceOf[T]
+    case TypeTag.Byte => config.getBytes(key).asInstanceOf[T]
+    case TypeTag.Boolean => config.getBoolean(key).asInstanceOf[T]
+    case NUMBER_TYPE => config.getNumber(key).asInstanceOf[T]
+    case OBJECT_TYPE => config.getObject(key).asInstanceOf[T]
+    case VALUE_TYPE => config.getValue(key).asInstanceOf[T]
+    case ANY_REF_TYPE => config.getAnyRef(key).asInstanceOf[T]
+
+    case INT_LIST_TYPE => config.getIntList(key).asInstanceOf[T]
+    case DOUBLE_LIST_TYPE => config.getDoubleList(key).asInstanceOf[T]
+    case BOOL_LIST_TYPE => config.getBooleanList(key).asInstanceOf[T]
+    case LONG_LIST_TYPE => config.getLongList(key).asInstanceOf[T]
     case _ => throw new UnsupportedOperationException(s"$tag is not supported yet")
   }
 
   val STRING_TYPE = typeOf[String]
+  val NUMBER_TYPE = typeOf[Number]
+  val INT_LIST_TYPE = typeOf[List[Int]]
+  val BOOL_LIST_TYPE = typeOf[List[Boolean]]
+  val DOUBLE_LIST_TYPE = typeOf[List[Double]]
+  val LONG_LIST_TYPE = typeOf[List[Double]]
+  val OBJECT_TYPE = typeOf[ConfigObject]
+  val VALUE_TYPE = typeOf[ConfigValue]
+  val ANY_REF_TYPE = typeOf[AnyRef]
 }
 
 /**
