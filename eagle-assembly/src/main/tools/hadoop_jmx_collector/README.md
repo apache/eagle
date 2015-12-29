54f5dc76643aa914c57c5c6675b4637e2d908026
Zhao, Qingwen <qingwzhao@ebay.com>
2015-12-29 11:20:06 +0800

    add license

--- eagle-assembly/src/main/tools/hadoop_jmx_collector/README.md
+++ eagle-assembly/src/main/tools/hadoop_jmx_collector/README.md
@@ -1,6 +1,26 @@
+<!--
+{% comment %}
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to you under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+{% endcomment %}
+-->
+
+
 # Hadoop Jmx Collector
 
-These scripts help to collect Hadoop jmx and evently sent the metrics to stdout or Kafka. 
+These scripts help to collect Hadoop jmx and evently sent the metrics to stdout or Kafka. Tested with Python 2.7.
 
 ## How to use it
   
--- eagle-assembly/src/main/tools/hadoop_jmx_collector/hadoop_jmx_kafka.py
+++ eagle-assembly/src/main/tools/hadoop_jmx_collector/hadoop_jmx_kafka.py
@@ -1,7 +1,20 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 #!/usr/bin/python
-# Parse jmxjson output from hadoop/hbase component
-# Author: Qingwen Zhao, qingwzhao@ebay.com, Hadoop Engineering Team, eBay Inc.
-# Python version: 2.7
 
 
 import os
@@ -61,7 +74,7 @@ def loadConfigFile(filename):
         config = json.loads(json_file)
 
     except ValueError:
-        print "cronus.ini error loading"
+        print "configuration file load error"
     return config
 
 
@@ -211,8 +224,6 @@ def main():
         config = loadConfigFile('eagle-collector.conf')
         #print config
 
-        cluster = config[u'env'].get('cluster').encode('utf-8')
-        datacenter = config[u'env'].get('datacenter').encode('utf-8')
         site = config[u'env'].get('site').encode('utf-8')
         host = socket.getfqdn()
 
