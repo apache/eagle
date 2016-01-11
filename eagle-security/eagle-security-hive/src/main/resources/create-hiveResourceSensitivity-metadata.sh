dd26aa430194f489c475432bf34978d454e494b5
Hao Chen <hao@apache.org>
2016-01-11 13:14:52 +0800

    EAGLE-114 Enable RAT check and fix missing license headers
    https://issues.apache.org/jira/browse/EAGLE-114
    
    - Enable rat check in pom
    - Added missing headers in java sources, shell scripts, config and doc
    files. Some of the test files and json files are excluded since these
    formats don't support comments.
    - The files under webapp in eagle-webservice module are deferred in this
    patch. Those will tracked in a separate patch.
    - Compilation, rat check (via maven verify) and unit tests pass with the
    patch.
    
    Author: @pmujumdar  <prasadm@cloudera.com>
    Reviewer: @haoch <prasadm@apache.org>
    
    Closes #51

--- eagle-security/eagle-security-hive/src/main/resources/create-hiveResourceSensitivity-metadata.sh
+++ eagle-security/eagle-security-hive/src/main/resources/create-hiveResourceSensitivity-metadata.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #### HiveResourceSensitivityService
 curl -X POST -H 'Content-Type:application/json' "http://localhost:38080/eagle-service/rest/entities?serviceName=HiveResourceSensitivityService" -d '[{"prefix":"hiveResourceSensitivity","tags":{"hiveResource":"xademo.customer_details.phone_number"},"sensitivityType":"PHONE_NUMBER"}]'
