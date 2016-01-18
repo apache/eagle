5517818176565a1833b6d63fbc89eaf15e2051fb
Hao Chen <hchen9@ebay.com>
2016-01-18 14:11:55 +0800

    Remove eagle-stream-pipeline-scheduler into EAGLE-98

--- eagle-core/eagle-data-process/eagle-stream-pipeline-scheduler/src/main/scala/org/apache/eagle/stream/dsl/scheduler/model/EntityConversion.scala
+++
@@ -1,9 +0,0 @@
-package org.apache.eagle.stream.dsl.scheduler.model
-
-import org.apache.eagle.log.base.taggedlog.TaggedLogAPIEntity
-
-
-private[execution] trait EntityConversion[M <: TaggedLogAPIEntity] {
-  def toEntity: M
-  def fromEntity (m: M)
-}
