31a9e8c926efefd8646185e6ab31a0bb519b4589
Ralph, Su <suliangfei@gmail.com>
2016-01-19 19:40:21 +0800

    add topology init shell

--- eagle-hadoop-metric/src/main/java/org/apache/eagle/hadoop/metric/NameNodeLagMonitor.java
+++ eagle-hadoop-metric/src/main/java/org/apache/eagle/hadoop/metric/NameNodeLagMonitor.java
@@ -20,9 +20,6 @@ import org.apache.eagle.dataproc.impl.storm.kafka.KafkaSourcedSpoutProvider;
 import org.apache.eagle.datastream.ExecutionEnvironments;
 import org.apache.eagle.datastream.core.StreamProducer;
 import org.apache.eagle.datastream.storm.StormExecutionEnvironment;
-import org.apache.eagle.partition.PartitionStrategy;
-
-import java.util.Arrays;
 
 /**
  * Created on 1/12/16.
@@ -33,19 +30,7 @@ public class NameNodeLagMonitor {
         StormExecutionEnvironment env = ExecutionEnvironments.get(args, StormExecutionEnvironment.class);
         String streamName = "s";
         StreamProducer sp = env.fromSpout(new KafkaSourcedSpoutProvider()).parallelism(1).nameAs(streamName);
-        sp.aggregateDirect(Arrays.asList(streamName),
-                "define stream s (host string, timestamp long, metric string, component string, site string, value long);" +
-                        " from s[metric=='hadoop.namenode.dfs.lastwrittentransactionid' and host=='a' ]#window.externalTime(timestamp, 5 min) select * insert into tmp1;" +
-                        " from s[metric=='hadoop.namenode.dfs.lastwrittentransactionid' and host=='b' ]#window.externalTime(timestamp, 5 min) select * insert into tmp2;" +
-                        " from tmp1 , tmp2 select tmp1.timestamp as t1time, max(tmp1.value) - max(tmp2.value) as gap insert into tmp3;" +
-                        " from tmp3[gap > 100] insert into tmp;"
-                ,
-                new PartitionStrategy() {
-                    @Override
-                    public int balance(String key, int buckNum) {
-                        return 0;
-                    }
-                });
+        sp.alertWithConsumer(streamName, "hadoopJmxMetricAlertExecutor");
 
         env.execute();
     }
