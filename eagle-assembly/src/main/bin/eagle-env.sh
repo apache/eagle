f4b36162d2f43283ab170c08085c86295ca24993
Zhao, Qingwen <qingwzhao@ebay.com>
2016-01-28 15:04:42 +0800

    add eagle-drop-table.sh

--- eagle-assembly/src/main/bin/eagle-env.sh
+++ eagle-assembly/src/main/bin/eagle-env.sh
@@ -45,3 +45,4 @@ done
 
 # EAGLE_TABLE_LIST
 export EAGLE_TABLE_LIST='alertdef ipzone streamMetadata alertdetail fileSensitivity eaglehdfs_alert streamdef eagle_metric alertExecutor alertStream alertStreamSchema hiveResourceSensitivity hbaseResourceSensitivity mlmodel userprofile hfdsusercommandpattern appCommand appDefinition serviceAudit aggregatedef eagleSiteDesc eagleSiteApplication eagleApplicationDesc eagleFeatureDesc'
+
