9a9ff6888df74a3fa6f68c6e2bbf6d022c79481a
yonzhang <yonzhang@ebay.com>
2016-02-10 12:50:49 -0800

    automated hadoop metric

--- eagle-assembly/src/main/bin/hadoop-metric-monitor.sh
+++ eagle-assembly/src/main/bin/hadoop-metric-monitor.sh
@@ -18,26 +18,33 @@
 source $(dirname $0)/eagle-env.sh
 
 ######################################################################
-##            Create Kafka topic for HADOOP METRIC
-######################################################################
-
-
-######################################################################
 ##            Import stream metadata for HADOOP METRIC
 ######################################################################
-./hadoop-metric-init.sh
+${EAGLE_HOME}/bin/hadoop-metric-init.sh
 
 ######################################################################
 ##            Run topology for HADOOP METRIC
 ######################################################################
-./eagle-topology.sh --main org.apache.eagle.hadoop.metric.HadoopJmxMetricMonitor --topology sandbox-hadoopjmx-topology --config ../conf/sandbox-hadoopjmx-topology.conf start
+### if topology exists, we should shutdown it
+echo "check topology status ..."
+active=$(${EAGLE_HOME}/bin/eagle-topology.sh --topology sandbox-hadoopjmx-topology status | grep ACTIVE)
+echo "topology status $active"
+if [ "$active" ]; then
+ echo "stop topology ..."
+ ${EAGLE_HOME}/bin/eagle-topology.sh --topology sandbox-hadoopjmx-topology stop
+fi
+echo "start Eagle Hadoop Metric Monitoring topology"
+${EAGLE_HOME}/bin/eagle-topology.sh --main org.apache.eagle.hadoop.metric.HadoopJmxMetricMonitor --topology sandbox-hadoopjmx-topology --config ../conf/sandbox-hadoopjmx-topology.conf start
 
 ######################################################################
 ##            Setup minutely crontab job for HADOOP METRIC
 ######################################################################
+echo "set up crontab script"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cp $DIR/../tools/hadoop_jmx_collector/config-sample.json $DIR/../tools/hadoop_jmx_collector/config.json
 command="python $DIR/../tools/hadoop_jmx_collector/hadoop_jmx_kafka.py"
 echo $job
 job="* * * * * $command >> $DIR/../logs/hadoop_metric.log"
 cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
+
+echo "$(crontab -l)"
