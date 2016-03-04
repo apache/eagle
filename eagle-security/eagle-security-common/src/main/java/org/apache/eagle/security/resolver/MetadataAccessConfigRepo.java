0a9ace999a9e811de7d0d7bda2f2f81ef8ec4f52
Zhao, Qingwen <qingwzhao@ebay.com>
2016-03-04 09:55:50 +0800

    enable kerberos

---
+++ eagle-security/eagle-security-common/src/main/java/org/apache/eagle/security/resolver/MetadataAccessConfigRepo.java
@@ -0,0 +1,7 @@
+package org.apache.eagle.security.resolver;
+
+/**
+ * Created by qingwzhao on 3/3/16.
+ */
+public class MetadataAccessConfigRepo {
+}
---
+++ eagle-security/eagle-security-common/src/main/java/org/apache/eagle/security/util/HadoopSecurityUtil.java
@@ -0,0 +1,7 @@
+package org.apache.eagle.security.util;
+
+/**
+ * Created by qingwzhao on 3/3/16.
+ */
+public class HadoopSecurityUtil {
+}
