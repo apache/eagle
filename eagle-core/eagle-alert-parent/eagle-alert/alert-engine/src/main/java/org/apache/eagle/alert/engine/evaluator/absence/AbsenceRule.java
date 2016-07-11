1dffec09ccb084c484e0427d2ce2cb567403cf21
yonzhang <yonzhang2012@gmail.com>
2016-07-11 16:56:46 -0700

    EAGLE-370 absence alert engine absence alert engine
    Author: Yong Zhang <yonzhang2012@apache.org>
    Reviewer: Yong Zhang
    
    Closes: #262

--- eagle-core/eagle-alert-parent/eagle-alert/alert-engine/src/main/java/org/apache/eagle/alert/engine/evaluator/absence/AbsenceRule.java
+++ eagle-core/eagle-alert-parent/eagle-alert/alert-engine/src/main/java/org/apache/eagle/alert/engine/evaluator/absence/AbsenceRule.java
@@ -1,30 +1,23 @@
-/*
+/**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.eagle.ml;
+package org.apache.eagle.alert.engine.evaluator.absence;
 
-import org.apache.eagle.ml.model.MLModelAPIEntity;
-
-import java.util.List;
-
-public interface MLModelDAO {
-    /**
-     * @param user
-     * @param algorithm
-     * @return
-     */
-	List<MLModelAPIEntity> findMLModelByContext(String user, String algorithm);
+/**
+ * Since 7/7/16.
+ */
+public interface AbsenceRule {
 }
