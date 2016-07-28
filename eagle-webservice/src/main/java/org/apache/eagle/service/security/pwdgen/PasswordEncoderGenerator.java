4d7ccd18e817ac65e81fe80ca992801b6ed90174
anyway1021 <mw@apache.org>
2016-07-28 11:12:46 +0800

    fix typo in files

--- eagle-webservice/src/main/java/org/apache/eagle/service/security/pwdgen/PasswordEncoderGenerator.java
+++ eagle-webservice/src/main/java/org/apache/eagle/service/security/pwdgen/PasswordEncoderGenerator.java
@@ -20,7 +20,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordEncoderGenerator {
     public static void main(String[] args) {
-        String password = "eagle@ebay.com";
+        String password = "eagle@fake.pwd";
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String hashedPassword = passwordEncoder.encode(password);
         System.out.println(hashedPassword);
