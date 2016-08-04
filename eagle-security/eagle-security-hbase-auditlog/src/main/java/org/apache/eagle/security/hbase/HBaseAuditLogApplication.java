efece0d5346694caba042f35d8450707fd7dfc32
yonzhang <yonzhang2012@gmail.com>
2016-08-04 12:42:46 -0700

    upgrade siddhi to 3.1.1 for external time batch

--- eagle-security/eagle-security-hbase-auditlog/src/main/java/org/apache/eagle/security/hbase/HBaseAuditLogApplication.java
+++ eagle-security/eagle-security-hbase-auditlog/src/main/java/org/apache/eagle/security/hbase/HBaseAuditLogApplication.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.eagle.security.hbase;
 
 import backtype.storm.topology.BoltDeclarer;
@@ -6,8 +22,6 @@ import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.tuple.Fields;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
-import org.apache.eagle.app.AbstractApplication;
-import org.apache.eagle.app.ApplicationContext;
 import org.apache.eagle.security.topo.NewKafkaSourcedSpoutProvider;
 import storm.kafka.StringScheme;
 import storm.kafka.bolt.KafkaBolt;
@@ -15,13 +29,12 @@ import storm.kafka.bolt.KafkaBolt;
 /**
  * Since 7/27/16.
  */
-public class HBaseAuditLogApplication extends AbstractApplication{
+public class HBaseAuditLogApplication{
     public final static String SPOUT_TASK_NUM = "topology.numOfSpoutTasks";
     public final static String PARSER_TASK_NUM = "topology.numOfParserTasks";
     public final static String JOIN_TASK_NUM = "topology.numOfJoinTasks";
     public final static String SINK_TASK_NUM = "topology.numOfSinkTasks";
-    @Override
-    protected void buildApp(TopologyBuilder builder, ApplicationContext context) {
+    protected void buildApp(TopologyBuilder builder) {
         System.setProperty("config.resource", "/application.conf");
         Config config = ConfigFactory.load();
         NewKafkaSourcedSpoutProvider provider = new NewKafkaSourcedSpoutProvider();
