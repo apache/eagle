00597dec64ae1473ba19eb3c033bc0a8c46c33fe
Xiancheng Li <xiancheng.li@ebay.com>
2016-08-05 12:43:27 +0800

    [EAGLE-435] make coordiantor schedule operation exclusive in distributed deployment

--- eagle-core/eagle-alert-parent/eagle-alert/alert-coordinator/src/main/java/org/apache/eagle/alert/coordinator/CoordinatorListener.java
+++ eagle-core/eagle-alert-parent/eagle-alert/alert-coordinator/src/main/java/org/apache/eagle/alert/coordinator/CoordinatorListener.java
@@ -19,8 +19,22 @@ package org.apache.eagle.alert.coordinator;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.leader.LeaderSelector;
+import org.apache.curator.framework.recipes.leader.LeaderSelectorListener;
+import org.apache.curator.framework.recipes.leader.LeaderSelectorListenerAdapter;
+import org.apache.curator.framework.state.ConnectionState;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.curator.utils.CloseableUtils;
+import org.apache.eagle.alert.config.ZKConfig;
+import org.apache.eagle.alert.config.ZKConfigBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 
 /**
  * @since Jun 16, 2016
@@ -29,16 +43,55 @@ import org.slf4j.LoggerFactory;
 public class CoordinatorListener implements ServletContextListener {
     
     private static final Logger LOG = LoggerFactory.getLogger(CoordinatorListener.class);
-
+    
+    private static final String PATH = "/alertleader";
+    private static final String COORDINATOR = "coordinator";
+    
+    private final Config config;
+    
+    private LeaderSelector selector;
+    private CuratorFramework client;
+    
+    public CoordinatorListener() {
+    	config = ConfigFactory.load().getConfig(COORDINATOR);
+    }
+    
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        LOG.info("start coordinator background tasks..");
-        Coordinator.startSchedule();
-    }
+        LeaderSelectorListener listener = new LeaderSelectorListenerAdapter() {
+        	
+        	@Override
+            public void takeLeadership(CuratorFramework client) throws Exception {
+                // this callback will get called when you are the leader
+                // do whatever leader work you need to and only exit
+                // this method when you want to relinquish leadership
+            	LOG.info("this is leader node right now..");
+            	LOG.info("start coordinator background tasks..");
+                Coordinator.startSchedule();
+                
+                Thread.currentThread().join();
+            }
+            
+        	@Override
+            public void stateChanged(CuratorFramework client, ConnectionState newState) {
+            	LOG.info(String.format("leader selector listener, new state: %s", newState.toString()));
+            }
 
+        };
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        ZKConfig zkConfig = ZKConfigBuilder.getZKConfig(config);
+        client = CuratorFrameworkFactory.newClient(zkConfig.zkQuorum, retryPolicy);
+        client.start();
+        
+        selector = new LeaderSelector(client, PATH, listener);
+        selector.autoRequeue();  // not required, but this is behavior that you will probably expect
+        selector.start();
+    }
+    
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-
+    	CloseableUtils.closeQuietly(selector);
+    	CloseableUtils.closeQuietly(client);
     }
 
 }
