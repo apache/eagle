936fc5a4eff3cd5bea11c1d15935b124b58b3780
Daniel <Daniel.Zhou@dataguise.com>
2016-08-09 15:39:31 -0700

    EAGLE-283: Convert fid,uid in MapR's audit log to FIle/folder name, user name
    1. Added resolver for user's input of "volume", "file/folder", "Status"
    	- These resolver provide drop down list for user to select
    
    2. Added NameConvertor to convert user's input to corresponding id and saved it in policy
    	- volume value will be used to call MapR's REST API to get volume id
    	- status code will be resolved to status id
    	- file/folder name will be passed to MapR's command line class to get its id.
    
    3. Modified UI js file
    	- As soon as user input a value for "volume", "file", "status", it will fetch user's input and call rest service to get corresponding id
    	- Save ids as a new match criteria in the policy
    
    4. Added maprfs resource resolver to provide drop down list for user
    
    PS:how the id converter works: user select "== '/test'" in field "dst", a new match criteria "== 234.567.890" will appear automatically under it, and the policy will end up as:
          dst == 'test'	or dst == '234.567.890'
    Same thing will happen for volume and status input.
    In this way, the policy is human readable and can be used to match the ids in maprfsAuditLog.

---
+++ eagle-security/eagle-security-maprfs-web/src/main/java/org/apache/eagle/service/security/hdfs/resolver/MAPRFSVolumeResolver.java
@@ -0,0 +1,99 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.eagle.service.security.hdfs.resolver;
+
+import com.typesafe.config.Config;
+import org.apache.eagle.security.resolver.MetadataAccessConfigRepo;
+import org.apache.eagle.service.alert.resolver.AttributeResolvable;
+import org.apache.eagle.service.alert.resolver.AttributeResolveException;
+import org.apache.eagle.service.alert.resolver.BadAttributeResolveRequestException;
+import org.apache.eagle.service.alert.resolver.GenericAttributeResolveRequest;
+import org.apache.eagle.service.security.hdfs.MAPRFSResourceConstants;
+import org.apache.eagle.service.security.hdfs.rest.HttpRequest;
+import org.apache.hadoop.conf.Configuration;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class MAPRFSVolumeResolver implements AttributeResolvable<GenericAttributeResolveRequest,String> {
+
+    private final static Logger LOG = LoggerFactory.getLogger(MAPRFSVolumeResolver.class);
+
+    @Override
+    public List<String> resolve(GenericAttributeResolveRequest request) throws AttributeResolveException {
+        String query = request.getQuery().trim();
+        List<String> volumelist = new ArrayList<>();
+        MetadataAccessConfigRepo repo = new MetadataAccessConfigRepo();
+        //Call MAPR REST API to get volumes
+        try {
+            Config config = repo.getConfig(MAPRFSResourceConstants.MAPRFS_APPLICATION, request.getSite().trim());
+            Configuration conf = repo.convert(config);
+            //get user name, password, make request to mapr rest service
+            String username;
+            String password;
+            username = config.getString(MAPRFSResourceConstants.MAPRFS_USERNAME);
+            password = config.getString(MAPRFSResourceConstants.MAPRFS_PASSWORD);
+            //constuct url to query mapr volume
+            String restUrl = config.getString(MAPRFSResourceConstants.MAPRFS_WEBUI_HTTPS) + MAPRFSResourceConstants.MAPRFS_LIST_VOLUME;
+
+            JSONObject response = HttpRequest.executeGet(restUrl,username,password);
+            volumelist = extractVolumeList(response);
+            List<String> res = new ArrayList<>();
+            for(String status : volumelist) {
+                Pattern pattern = Pattern.compile("^" + query, Pattern.CASE_INSENSITIVE);
+                if(pattern.matcher(status).find()) {
+                    res.add(status);
+                }
+            }
+            if(res.size() == 0) {
+                return volumelist;
+            }
+            return res;
+        } catch( Exception e ) {
+            LOG.error(" Exception in MAPRFS Volume Resolver ", e);
+            throw new AttributeResolveException(e);
+        }
+    }
+
+    @Override
+    public void validateRequest(GenericAttributeResolveRequest request) throws BadAttributeResolveRequestException {
+        ;
+    }
+
+    @Override
+    public Class<GenericAttributeResolveRequest> getRequestClass() {
+        return GenericAttributeResolveRequest.class;
+    }
+
+
+    public List<String> extractVolumeList(JSONObject response){
+        // rest url:  https://192.168.6.148:8443/rest/volume/list
+        List<String> result = new ArrayList<>();
+        JSONArray list = (JSONArray) response.get("data");
+        for(int i = 0; i< list.length();i++ ){
+            JSONObject element = (JSONObject) list.get(i);
+            result.add(element.getString("volumename"));
+        }
+        return result;
+    }
+
+}
