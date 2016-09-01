2bd1da55af8565d7a64c542d93f2261c447fee0b
jiljiang <jiljiang@ebay.com>
2016-09-01 16:15:56 +0800

    job task duration distribution

--- eagle-webservice/src/main/webapp/app/apps/jpm/ctrl/jobTaskCtrl.js
+++ eagle-webservice/src/main/webapp/app/apps/jpm/ctrl/jobTaskCtrl.js
@@ -23,7 +23,10 @@
 	 */
 	register(function (jpmApp) {
 		var TREND_INTERVAL = 60;
-		var BUCKET_COUNT = 30;
+		var SCHEDULE_BUCKET_COUNT = 30;
+		var TASK_STATUS = ["SUCCEEDED", "FAILED", "KILLED"];
+		var TASK_TYPE = ["MAP", "REDUCE"];
+		var DURATION_BUCKETS = [0, 30 * 1000, 60 * 1000, 120 * 1000, 300 * 1000, 600 * 1000, 1800 * 1000, 3600 * 1000, 2 * 3600 * 1000, 3 * 3600 * 1000];
 		var TASK_FIELDS = [
 			"rack",
 			"hostname",
@@ -34,6 +37,46 @@
 			"endTime",
 			"jobCounters"
 		];
+
+		function getCommonHeatMapOption(categoryList, maxCount) {
+			return {
+				animation: false,
+				tooltip: {
+					trigger: 'item'
+				},
+				xAxis: {splitArea: {show: true}},
+				yAxis: [{
+					type: 'category',
+					data: categoryList,
+					splitArea: {show: true},
+					axisTick: {show: false}
+				}],
+				grid: { bottom: "50" },
+				visualMap: {
+					min: 0,
+					max: maxCount,
+					calculable: true,
+					orient: 'horizontal',
+					left: 'right',
+					inRange: {
+						color: ["#00a65a", "#ffdc62", "#dd4b39"]
+					}
+				}
+			};
+		}
+
+		function getCommonHeatMapSeries(name, data) {
+			return {
+				name: name,
+				type: "heatmap",
+				data: data,
+				itemStyle: {
+					normal: {
+						borderColor: "#FFF"
+					}
+				}
+			};
+		}
 
 		/**
 		 * @typedef {{}} Task
@@ -48,6 +91,7 @@
 		 * @property {number} _bucketStart
 		 * @property {number} _bucketEnd
 		 * @property {number} _duration
+		 * @property {number} _durationBucket
 		 */
 
 		jpmApp.controller("jobTaskCtrl", function ($wrapState, $scope, PageConfig, Time, JPM) {
@@ -61,94 +105,33 @@
 			PageConfig.subTitle = $scope.jobId;
 
 			var timeDiff = endTime - startTime;
-			var timeDes = Math.ceil(timeDiff / BUCKET_COUNT);
+			var timeDes = Math.ceil(timeDiff / SCHEDULE_BUCKET_COUNT);
 
-			$scope.bucketCategory = [];
-			for(var i = 0 ; i < BUCKET_COUNT ; i += 1) {
-				$scope.bucketCategory.push(Time.diffStr(i * timeDes) + "\n~\n" + Time.diffStr((i + 1) * timeDes));
+			$scope.bucketScheduleCategory = [];
+			for(var i = 0 ; i < SCHEDULE_BUCKET_COUNT ; i += 1) {
+				$scope.bucketScheduleCategory.push(Time.format(startTime + i * timeDes, "HH:mm:SS") + "\n~\n" + Time.format(startTime + (i + 1) * timeDes, "HH:mm:SS"));
 			}
 
-			// ==========================================================================
-			// =                               Fetch Task                               =
-			// ==========================================================================
+			$scope.bucketDurationCategory = [];
+			$.each(DURATION_BUCKETS, function (i, start) {
+				var end = DURATION_BUCKETS[i + 1];
+				if(!start) {
+					$scope.bucketDurationCategory.push("<" + Time.diffStr(end));
+				} else if(!end) {
+					$scope.bucketDurationCategory.push(">" + Time.diffStr(start));
+				} else {
+					$scope.bucketDurationCategory.push(Time.diffStr(start) + "\n~\n" + Time.diffStr(end));
+				}
+			});
+
+			// ============================================================================
+			// ============================================================================
+			// ==                               Fetch Task                               ==
+			// ============================================================================
+			// ============================================================================
 			$scope.list = JPM.list("TaskExecutionService", {site: $scope.site, jobId: $scope.jobId}, startTime, endTime, TASK_FIELDS, 1000000);
 			$scope.list._promise.then(function () {
 				var i;
-
-				function getHeatMapOption(categoryList, maxCount) {
-					return {
-						animation: false,
-						tooltip: {
-							trigger: 'item',
-							formatter: function (point) {
-								if(point.data) {
-									return categoryList[point.data[1]] + ":<br/>" +
-										'<span style="display:inline-block;margin-right:5px;border-radius:10px;width:9px;height:9px;background-color:' + point.color + '"></span> ' +
-										$scope.bucketCategory[point.data[0]] + ": " +
-										point.data[2];
-								}
-								return "";
-							}
-						},
-						xAxis: {splitArea: {show: true}},
-						yAxis: [{
-							type: 'category',
-							data: categoryList,
-							splitArea: {show: true},
-							axisTick: {show: false}
-						}],
-						grid: { bottom: "50" },
-						visualMap: {
-							min: 0,
-							max: maxCount,
-							calculable: true,
-							orient: 'horizontal',
-							left: 'right',
-							inRange: {
-								color: ["#00a65a", "#ffdc62", "#dd4b39"]
-							}
-						}
-					};
-				}
-
-				function fillBucket(countList, task, maxCount) {
-					for(var bucketId = task._bucketStart ; bucketId <= task._bucketEnd ; bucketId += 1) {
-						var count = countList[bucketId] = (countList[bucketId] || 0) + 1;
-						maxCount = Math.max(maxCount, count);
-					}
-					return maxCount;
-				}
-
-				function bucketToSeries(categoryList, buckets, name) {
-					var bucket_data = $.map(categoryList, function (category, index) {
-						var list = [];
-						var dataList = buckets[category] || [];
-						for(var i = 0 ; i < BUCKET_COUNT ; i += 1) {
-							list.push([i, index, dataList[i] || 0]);
-						}
-						return list;
-					});
-
-					return [{
-						name: name,
-						type: "heatmap",
-						data: bucket_data,
-						itemStyle: {
-							normal: {
-								borderColor: "#FFF"
-							}
-						},
-						label: {
-							normal: {
-								show: true,
-								formatter: function (point) {
-									if(point.data[2] === 0) return "-";
-									return " ";
-								}
-							}
-						}
-					}];
-				}
 
 				// ========================= Schedule Trend =========================
 				var trend_map_countList = [];
@@ -202,15 +185,64 @@
 					 * @param {Task} task
 					 */
 					function (i, task) {
-						task._bucket = Math.floor((task.startTime - startTime + (task.endTime - task.startTime) / 2) / timeDes);
 						task._bucketStart = Math.floor((task.startTime - startTime) / timeDes);
 						task._bucketEnd = Math.floor((task.endTime - startTime) / timeDes);
 						task._duration = task.endTime - task.startTime;
+						task._durationBucket = common.number.inRange(DURATION_BUCKETS, task._duration);
 					});
 
-				// ======================== Status Statistic ========================
-				var TASK_STATUS = ["SUCCEEDED", "FAILED", "KILLED"];
+				// ==================================================================
+				// =                      Schedule Distribution                     =
+				// ==================================================================
+				function fillBucket(countList, task, maxCount) {
+					for(var bucketId = task._bucketStart ; bucketId <= task._bucketEnd ; bucketId += 1) {
+						var count = countList[bucketId] = (countList[bucketId] || 0) + 1;
+						maxCount = Math.max(maxCount, count);
+					}
+					return maxCount;
+				}
 
+				function getHeatMapOption(categoryList, maxCount) {
+					var option = getCommonHeatMapOption(categoryList, maxCount);
+					return common.merge(option, {
+						tooltip: {
+							formatter: function (point) {
+								if(point.data) {
+									return categoryList[point.data[1]] + ":<br/>" +
+										'<span style="display:inline-block;margin-right:5px;border-radius:10px;width:9px;height:9px;background-color:' + point.color + '"></span> ' +
+										$scope.bucketScheduleCategory[point.data[0]] + ": " +
+										point.data[2];
+								}
+								return "";
+							}
+						}
+					});
+				}
+
+				function bucketToSeries(categoryList, buckets, name) {
+					var bucket_data = $.map(categoryList, function (category, index) {
+						var list = [];
+						var dataList = buckets[category] || [];
+						for(var i = 0 ; i < SCHEDULE_BUCKET_COUNT ; i += 1) {
+							list.push([i, index, dataList[i] || 0]);
+						}
+						return list;
+					});
+
+					return [common.merge(getCommonHeatMapSeries(name, bucket_data), {
+						label: {
+							normal: {
+								show: true,
+								formatter: function (point) {
+									if(point.data[2] === 0) return "-";
+									return " ";
+								}
+							}
+						}
+					})];
+				}
+
+				// ======================== Status Statistic ========================
 				var bucket_status = {};
 				var bucket_status_maxCount = 0;
 				$.each($scope.list,
@@ -354,6 +386,165 @@
 
 				$scope.localWriteSeries = bucketToSeries(TASK_LOCAL_DISTRIBUTION, bucket_local_writes, "Task Local Write Distribution");
 				$scope.localWriteOption = getHeatMapOption(TASK_LOCAL_DISTRIBUTION, bucket_local_writes_maxCount);
+
+				// ==================================================================
+				// =                      Duration Distribution                     =
+				// ==================================================================
+				function fillDurationBucket(countList, task, maxCount) {
+					var count = countList[task._durationBucket] = (countList[task._durationBucket] || 0) + 1;
+					maxCount = Math.max(maxCount, count);
+					return maxCount;
+				}
+
+				function getDurationHeatMapOption(categoryList, maxCount) {
+					var option = getCommonHeatMapOption(categoryList, maxCount);
+					return common.merge(option, {
+						tooltip: {
+							formatter: function (point) {
+								if(point.data) {
+									return categoryList[point.data[1]] + ":<br/>" +
+										'<span style="display:inline-block;margin-right:5px;border-radius:10px;width:9px;height:9px;background-color:' + point.color + '"></span> ' +
+										$scope.bucketDurationCategory[point.data[0]] + ": " +
+										point.data[2];
+								}
+								return "";
+							}
+						}
+					});
+				}
+
+				function bucketToDurationSeries(categoryList, buckets, name) {
+					var bucket_data = $.map(categoryList, function (category, index) {
+						var list = [];
+						var dataList = buckets[category] || [];
+						for(var i = 0 ; i < DURATION_BUCKETS.length ; i += 1) {
+							list.push([i, index, dataList[i] || 0]);
+						}
+						return list;
+					});
+
+					return [common.merge(getCommonHeatMapSeries(name, bucket_data), {
+						label: {
+							normal: {
+								show: true,
+								formatter: function (point) {
+									if(point.data[2] === 0) return "-";
+									return point.data[2] + "";
+								}
+							}
+						}
+					})];
+				}
+
+				// ======================== Status Statistic ========================
+				var duration_status = {};
+				var duration_status_maxCount = 0;
+				$.each($scope.list,
+					/**
+					 * @param {number} i
+					 * @param {Task} task
+					 */
+					function (i, task) {
+						var countList = duration_status[task.taskStatus] = (duration_status[task.taskStatus] || []);
+
+						duration_status_maxCount = fillDurationBucket(countList, task, duration_status_maxCount);
+					});
+
+				$scope.durationStatusSeries = bucketToDurationSeries(TASK_STATUS, duration_status, "Task Status");
+				$scope.durationStatusOption = getDurationHeatMapOption(TASK_STATUS, duration_status_maxCount);
+
+				// ===================== Map / Reduce Statistic =====================
+				var mapReduce_status = {};
+				var mapReduce_status_maxCount = 0;
+				$.each($scope.list,
+					/**
+					 * @param {number} i
+					 * @param {Task} task
+					 */
+					function (i, task) {
+						var countList = mapReduce_status[task.tags.taskType] = (mapReduce_status[task.tags.taskType] || []);
+
+						mapReduce_status_maxCount = fillDurationBucket(countList, task, mapReduce_status_maxCount);
+					});
+
+				$scope.durationMapReduceSeries = bucketToDurationSeries(TASK_TYPE, mapReduce_status, "Task Type");
+				$scope.durationMapReduceOption = getDurationHeatMapOption(TASK_TYPE, mapReduce_status_maxCount);
+
+				// ======================= HDFS Read Statistic ======================
+				var duration_hdfs_reads = {};
+				var duration_hdfs_reads_maxCount = 0;
+
+				$.each($scope.list,
+					/**
+					 * @param {number} i
+					 * @param {Task} task
+					 */
+					function (i, task) {
+						var durationBucket = TASK_HDFS_DISTRIBUTION[common.number.inRange(TASK_HDFS_BYTES, task.jobCounters.counters["org.apache.hadoop.mapreduce.FileSystemCounter"].HDFS_BYTES_READ)];
+						var countList = duration_hdfs_reads[durationBucket] = (duration_hdfs_reads[durationBucket] || []);
+
+						duration_hdfs_reads_maxCount = fillDurationBucket(countList, task, duration_hdfs_reads_maxCount);
+					});
+
+				$scope.durationHdfsReadSeries = bucketToDurationSeries(TASK_HDFS_DISTRIBUTION, duration_hdfs_reads, "Task HDFS Read Distribution");
+				$scope.durationHdfsReadOption = getDurationHeatMapOption(TASK_HDFS_DISTRIBUTION, duration_hdfs_reads_maxCount);
+
+				// ====================== HDFS Write Statistic ======================
+				var duration_hdfs_writes = {};
+				var duration_hdfs_writes_maxCount = 0;
+
+				$.each($scope.list,
+					/**
+					 * @param {number} i
+					 * @param {Task} task
+					 */
+					function (i, task) {
+						var durationBucket = TASK_HDFS_DISTRIBUTION[common.number.inRange(TASK_HDFS_BYTES, task.jobCounters.counters["org.apache.hadoop.mapreduce.FileSystemCounter"].HDFS_BYTES_WRITTEN)];
+						var countList = duration_hdfs_writes[durationBucket] = (duration_hdfs_writes[durationBucket] || []);
+
+						duration_hdfs_writes_maxCount = fillDurationBucket(countList, task, duration_hdfs_writes_maxCount);
+					});
+
+				$scope.durationHdfsWriteSeries = bucketToDurationSeries(TASK_HDFS_DISTRIBUTION, duration_hdfs_writes, "Task HDFS Write Distribution");
+				$scope.durationHdfsWriteOption = getDurationHeatMapOption(TASK_HDFS_DISTRIBUTION, duration_hdfs_writes_maxCount);
+
+				// ====================== Local Read Statistic ======================
+				var duration_local_reads = {};
+				var duration_local_reads_maxCount = 0;
+
+				$.each($scope.list,
+					/**
+					 * @param {number} i
+					 * @param {Task} task
+					 */
+					function (i, task) {
+						var durationBucket = TASK_LOCAL_DISTRIBUTION[common.number.inRange(TASK_LOCAL_BYTES, task.jobCounters.counters["org.apache.hadoop.mapreduce.FileSystemCounter"].FILE_BYTES_READ)];
+						var countList = duration_local_reads[durationBucket] = (duration_local_reads[durationBucket] || []);
+
+						duration_local_reads_maxCount = fillDurationBucket(countList, task, duration_local_reads_maxCount);
+					});
+
+				$scope.durationLocalReadSeries = bucketToDurationSeries(TASK_LOCAL_DISTRIBUTION, duration_local_reads, "Task Local Read Distribution");
+				$scope.durationLocalReadOption = getDurationHeatMapOption(TASK_LOCAL_DISTRIBUTION, duration_local_reads_maxCount);
+
+				// ====================== Local Write Statistic =====================
+				var duration_local_writes = {};
+				var duration_local_writes_maxCount = 0;
+
+				$.each($scope.list,
+					/**
+					 * @param {number} i
+					 * @param {Task} task
+					 */
+					function (i, task) {
+						var durationBucket = TASK_LOCAL_DISTRIBUTION[common.number.inRange(TASK_HDFS_BYTES, task.jobCounters.counters["org.apache.hadoop.mapreduce.FileSystemCounter"].FILE_BYTES_WRITTEN)];
+						var countList = duration_local_writes[durationBucket] = (duration_local_writes[durationBucket] || []);
+
+						duration_local_writes_maxCount = fillDurationBucket(countList, task, duration_local_writes_maxCount);
+					});
+
+				$scope.durationLocalWriteSeries = bucketToDurationSeries(TASK_LOCAL_DISTRIBUTION, duration_local_writes, "Task Local Write Distribution");
+				$scope.durationLocalWriteOption = getDurationHeatMapOption(TASK_LOCAL_DISTRIBUTION, duration_local_writes_maxCount);
 			});
 		});
 	});
