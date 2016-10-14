806a29c15ad20fed8457ac39c52ac62f4c26641d
Hao Chen <hao@apache.org>
2016-10-14 08:24:11 +0800

    [MINOR] Simply reorg maven repositories
    Simply reorg maven repositories
    
    Author: Hao Chen <hao@apache.org>
    
    Closes #497 from haoch/FixRepo.

--- eagle-security/eagle-security-hbase-auditlog/src/main/java/org/apache/eagle/security/hbase/HBaseAuditLogAppProvider.java
+++ eagle-security/eagle-security-hbase-auditlog/src/main/java/org/apache/eagle/security/hbase/HBaseAuditLogAppProvider.java
@@ -31,11 +31,4 @@ public class HBaseAuditLogAppProvider extends AbstractApplicationProvider<HBaseA
     public HBaseAuditLogApplication getApplication() {
         return new HBaseAuditLogApplication();
     }
-
-    private static class MyModule extends AbstractModule {
-        @Override
-        protected void configure() {
-
-        }
-    }
 }
