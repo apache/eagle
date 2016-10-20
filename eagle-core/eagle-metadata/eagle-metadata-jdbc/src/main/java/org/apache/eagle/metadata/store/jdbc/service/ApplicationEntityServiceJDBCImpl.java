fa42f276f5c49146399f975620f44c81b47f4f25
wujinhu <wujinhu920@126.com>
2016-10-20 20:41:49 +0800

    fix metadata bug

--- eagle-core/eagle-metadata/eagle-metadata-jdbc/src/main/java/org/apache/eagle/metadata/store/jdbc/service/ApplicationEntityServiceJDBCImpl.java
+++ eagle-core/eagle-metadata/eagle-metadata-jdbc/src/main/java/org/apache/eagle/metadata/store/jdbc/service/ApplicationEntityServiceJDBCImpl.java
@@ -136,6 +136,8 @@ public class ApplicationEntityServiceJDBCImpl implements ApplicationEntityServic
         if (entity.getMode() != null && StringUtils.isNotBlank(entity.getMode().name())) {
             updateSql += "appmode = ?, ";
         }
+        entity.getContext().put("mode", entity.getMode().name());
+        entity.getContext().put("jarPath", entity.getJarPath());
         if (StringUtils.isNotBlank(entity.getJarPath())) {
             updateSql += "jarpath = ?, ";
         }
