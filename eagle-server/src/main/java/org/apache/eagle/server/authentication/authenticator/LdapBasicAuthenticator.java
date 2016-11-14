d80a5bc6c71a03aac4f801c6f035a6a1f94de022
anyway1021 <mw@apache.org>
2016-11-14 18:15:26 +0800

    implement ldap authentication logic integrated with Basic Authentication

--- eagle-server/src/main/java/org/apache/eagle/server/authentication/authenticator/LdapBasicAuthenticator.java
+++ eagle-server/src/main/java/org/apache/eagle/server/authentication/authenticator/LdapBasicAuthenticator.java
@@ -22,8 +22,18 @@ import io.dropwizard.auth.Authenticator;
 import io.dropwizard.auth.basic.BasicCredentials;
 import org.apache.eagle.common.authentication.User;
 import org.apache.eagle.server.authentication.config.LdapSettings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.naming.Context;
+import javax.naming.directory.InitialDirContext;
+import java.util.Hashtable;
 
 public class LdapBasicAuthenticator implements Authenticator<BasicCredentials, User> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LdapBasicAuthenticator.class);
+    private static final String LDAP_LDAP_CTX_FACTORY_NAME = "com.sun.jndi.ldap.LdapCtxFactory";
+    private static final String LDAP_CONNECT_TIMEOUT_KEY = "com.sun.jndi.ldap.connect.timeout";
+    private static final String LDAP_READ_TIMEOUT_KEY = "com.sun.jndi.ldap.read.timeout";
     private LdapSettings settings = null;
 
     public LdapBasicAuthenticator(LdapSettings settings) {
@@ -31,11 +41,46 @@ public class LdapBasicAuthenticator implements Authenticator<BasicCredentials, U
     }
 
     public Optional<User> authenticate(BasicCredentials credentials) throws AuthenticationException {
-        // TODO need to implement ldap authentication logic
-        boolean pass = true;
-        if (pass) {
-            return Optional.of(new User("ldap.username"));
+        String sanitizedUsername = sanitizeUsername(credentials.getUsername());
+        try {
+            new InitialDirContext(getContextEnvironment(sanitizedUsername, credentials.getPassword()));
+            return Optional.of(new User(sanitizedUsername));
+        } catch (javax.naming.AuthenticationException ae) {
+            LOGGER.warn(String.format("Authentication failed for user[%s]: wrong username or password", sanitizedUsername));
+            return Optional.absent();
+        } catch (Exception e) {
+            throw new AuthenticationException(String.format("Error occurs while trying to authenticate for user[%s]: %s", sanitizedUsername, e.getMessage()), e);
         }
-        return Optional.absent();
     }
+
+    private Hashtable<String, String> getContextEnvironment(String sanitizedUsername, String password) {
+        String providerUrl = settings.getProviderUrl();
+        if (providerUrl == null) {
+            throw new IllegalArgumentException("providerUrl of the ldap service shouldn't be null");
+        }
+
+        Hashtable<String, String> env = new Hashtable<>();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, LDAP_LDAP_CTX_FACTORY_NAME);
+        env.put(Context.PROVIDER_URL, providerUrl);
+        env.put(LDAP_CONNECT_TIMEOUT_KEY, String.valueOf(settings.getConnectingTimeout().toMilliseconds()));
+        env.put(LDAP_READ_TIMEOUT_KEY, String.valueOf(settings.getReadingTimeout().toMilliseconds()));
+
+        String strategy = settings.getStrategy();
+        if (!"".equals(strategy)) {
+            env.put(Context.SECURITY_AUTHENTICATION, strategy);
+        }
+
+        env.put(Context.SECURITY_PRINCIPAL, comprisePrincipal(sanitizedUsername));
+        env.put(Context.SECURITY_CREDENTIALS, password);
+        return env;
+    }
+
+    private String comprisePrincipal(String sanitizedUsername) {
+        return settings.getPrincipalTemplate().replaceAll("\\$\\{USERNAME\\}", sanitizedUsername);
+    }
+
+    private String sanitizeUsername(String username) {
+        return username.replaceAll("[^a-zA-Z0-9_.]", "");
+    }
+
 }
