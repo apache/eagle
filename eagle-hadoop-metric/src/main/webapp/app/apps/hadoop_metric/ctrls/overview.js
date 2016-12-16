5d8ca090648ea60292625b1e3c57f503e335b3fa
r7raul1984 <tangjijun@yhd.com>
2016-12-16 10:15:34 +0800

    Write a chart memusage demo.

--- eagle-hadoop-metric/src/main/webapp/app/apps/hadoop_metric/ctrls/overview.js
+++ eagle-hadoop-metric/src/main/webapp/app/apps/hadoop_metric/ctrls/overview.js
@@ -21,10 +21,25 @@
 	 * `register` without params will load the module which using require
 	 */
 	register(function (hadoopMetricApp) {
-		hadoopMetricApp.controller("overviewCtrl", function ($scope, PageConfig) {
-			PageConfig.title = 'Overview';
+		hadoopMetricApp.controller("overviewCtrl", function ($q, $wrapState, $scope, PageConfig, METRIC) {
+			var startTime = Number($wrapState.param.startTime);
+			var endTime = Number($wrapState.param.endTime);
+			$scope.site = $wrapState.param.siteId;
+			var jobCond = {
+				site: $scope.site
+			};
 
+			var METRIC_NAME = [
+				"hadoop.memory.nonheapmemoryusage.used",
+				"hadoop.memory.heapmemoryusage.used",
+				"hadoop.hbase.master.server.averageload",
+				"hadoop.hbase.master.assignmentmanger.ritcount",
+				"hadoop.hbase.master.assignmentmanger.ritcountoverthreshold"
+			];
+
+			PageConfig.title = 'Overview';
 			$scope.commonOption = {};
+			$scope.metricList = {};
 
 			// Mock series data
 			function mockMetric(name, option, count) {
@@ -32,13 +47,12 @@
 				var now = +new Date();
 
 				var series = [];
-				for (var i = 0 ; i < count ; i += 1) {
+				for (var i = 0; i < count; i += 1) {
 					var data = [];
 
-					for(var j = 0 ; j < 30 ; j += 1) {
+					for (var j = 0; j < 30; j += 1) {
 						data.push({x: now + j * 1000 * 60, y: Math.random() * 100});
 					}
-
 					series.push($.extend({
 						name: name + '_' + i,
 						type: 'line',
@@ -46,27 +60,68 @@
 						showSymbol: false,
 					}, option));
 				}
-
 				return {
 					title: name,
 					series: series
 				};
 			}
 
-			$scope.metricList = [
-				mockMetric('name1', {}),
-				mockMetric('name2', {smooth:true}),
-				mockMetric('name3', {areaStyle: {normal: {}}}),
-				mockMetric('name4', {type: 'bar'}),
-				mockMetric('name1', {}, 2),
-				mockMetric('name2', {smooth:true}, 2),
-				mockMetric('name3', {areaStyle: {normal: {}}, stack: 'one'}, 2),
-				mockMetric('name4', {type: 'bar', stack: 'one'}, 2),
-				mockMetric('name1', {}, 3),
-				mockMetric('name2', {smooth:true}, 3),
-				mockMetric('name3', {areaStyle: {normal: {}}, stack: 'one'}, 3),
-				mockMetric('name4', {type: 'bar', stack: 'one'}, 3),
-			];
+
+			function generateHbaseMetric(name, option, limit) {
+				limit = limit || 20;
+				var hbaseMetric;
+				var series = [];
+				$scope.site = $wrapState.param.siteId;
+				var jobCond = {
+					site: $scope.site
+				};
+				var data = [];
+				hbaseMetric = METRIC.hbaseMetrics(jobCond, name, limit);
+				return hbaseMetric._promise.then(function () {
+					data = $.map(hbaseMetric, function (metric) {
+						return {
+							x: metric.timestamp,
+							y: metric.value[0]
+						};
+					});
+
+					series.push($.extend({
+						name: name,
+						type: 'line',
+						data: data,
+						showSymbol: false,
+					}, option));
+					return {
+						title: name,
+						series: series
+					};
+				});
+			}
+
+			$q.all([
+				generateHbaseMetric(METRIC_NAME[0], {areaStyle: {normal: {}}}),
+				generateHbaseMetric(METRIC_NAME[1], {areaStyle: {normal: {}}}),
+				generateHbaseMetric(METRIC_NAME[2], {areaStyle: {normal: {}}}),
+				generateHbaseMetric(METRIC_NAME[3], {areaStyle: {normal: {}}})
+			]).then(function (res) {
+				console.log(res[0]);
+
+				$scope.metricList = [
+					res[0],
+					res[1],
+					res[2],
+					res[3],
+					mockMetric('name1', {}, 2),
+					mockMetric('name2', {smooth: true}, 2),
+					mockMetric('name3', {areaStyle: {normal: {}}, stack: 'one'}, 2),
+					mockMetric('name4', {type: 'bar', stack: 'one'}, 2),
+					mockMetric('name1', {}, 3),
+					mockMetric('name2', {smooth: true}, 3),
+					mockMetric('name3', {areaStyle: {normal: {}}, stack: 'one'}, 3),
+					mockMetric('name4', {type: 'bar', stack: 'one'}, 3)
+				]
+			});
 		});
 	});
 })();
+//@ sourceURL=overview.js
--- eagle-hadoop-metric/src/main/webapp/app/apps/hadoop_metric/index.js
+++ eagle-hadoop-metric/src/main/webapp/app/apps/hadoop_metric/index.js
@@ -34,13 +34,127 @@
 		controller: "hdfsCtrl",
 	});
 
-	hadoopMetricApp.portal({name: "Services", icon: "heartbeat", list: [
-		{name: "Overview", path: "hadoopMetric/"},
-		{name: "HDFS", path: "hadoopMetric/hdfs"},
-	]}, true);
+	hadoopMetricApp.portal({
+		name: "Services", icon: "heartbeat", list: [
+			{name: "Overview", path: "hadoopMetric/"},
+			{name: "HDFS", path: "hadoopMetric/hdfs"},
+		]
+	}, true);
+
+	hadoopMetricApp.service("METRIC", function ($q, $http, Time, Site, Application) {
+		var METRIC = window._METRIC = {};
+
+		METRIC.QUERY_HBASE_METRICS = '${baseURL}/rest/entities?query=GenericMetricService[${condition}]{*}&metricName=${metric}&pageSize=${limit}';
+		METRIC.QUERY_HBASE_METRICS_WITHTIME = '${baseURL}/rest/entities?query=GenericMetricService[${condition}]{*}&metricName=${metric}&pageSize=${limit}&startTime=${startTime}&endTime=${endTime}';;
+
+
+		/**
+		 * Fetch query content with current site application configuration
+		 * @param {string} queryName
+		 */
+		var getQuery = METRIC.getQuery = function (queryName, siteId) {
+			var baseURL;
+			siteId = siteId || Site.current().siteId;
+			var app = Application.find("HADOOP_METRIC_MONITOR", siteId)[0];
+			var host = app.configuration["service.host"];
+			var port = app.configuration["service.port"];
+
+			if (!host && !port) {
+				baseURL = "";
+			} else {
+				if (host === "localhost" || !host) {
+					host = location.hostname;
+				}
+				if (!port) {
+					port = location.port;
+				}
+				baseURL = "http://" + host + ":" + port;
+			}
+
+			return common.template(METRIC["QUERY_" + queryName], {baseURL: "http://192.168.7.192:9090"});
+		};
+
+		function wrapList(promise) {
+			var _list = [];
+			_list._done = false;
+
+			_list._promise = promise.then(
+				/**
+				 * @param {{}} res
+				 * @param {{}} res.data
+				 * @param {{}} res.data.obj
+				 */
+				function (res) {
+					_list.splice(0);
+					Array.prototype.push.apply(_list, res.data.obj);
+					_list._done = true;
+					return _list;
+				});
+			return _list;
+		}
+
+		function toFields(fields) {
+			return (fields || []).length > 0 ? $.map(fields, function (field) {
+				return "@" + field;
+			}).join(",") : "*";
+		}
+
+		METRIC.metricsToSeries = function (name, metrics, option, rawData) {
+			if (arguments.length === 4 && typeof rawData === "object") {
+				option = rawData;
+				rawData = false;
+			}
+
+			var data = $.map(metrics, function (metric) {
+				console.log(metric);
+				return rawData ? metric.value[0] : {
+					x: metric.timestamp,
+					y: metric.value[0]
+				};
+			});
+			return $.extend({
+				name: name,
+				showSymbol: false,
+				type: "line",
+				data: data
+			}, option || {});
+		};
+
+		METRIC.get = function (url, params) {
+			return $http({
+				url: url,
+				method: "GET",
+				params: params
+			});
+		};
+
+		METRIC.condition = function (condition) {
+			return $.map(condition, function (value, key) {
+				return "@" + key + '="' + value + '"';
+			}).join(" AND ");
+		};
+
+
+		METRIC.hbaseMetrics = function (condition, metric, limit) {
+			var config = {
+				condition: METRIC.condition(condition),
+				metric: metric,
+				limit: limit || 10000
+			};
+
+			var metrics_url = common.template(getQuery("HBASE_METRICS"), config);
+			var _list = wrapList(METRIC.get(metrics_url));
+			_list._promise.then(function () {
+				_list.reverse();
+			});
+			return _list;
+		};
+		return METRIC;
+	});
 
 	hadoopMetricApp.requireCSS("style/index.css");
 	hadoopMetricApp.require("widgets/availabilityChart.js");
 	hadoopMetricApp.require("ctrls/overview.js");
 	hadoopMetricApp.require("ctrls/hdfs.js");
 })();
+//# sourceURL=index.js
