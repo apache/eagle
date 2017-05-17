47e899121bb8fdbbc33b497e92c871ecd5394ffc
Zhao, Qingwen <qingwzhao@apache.org>
2017-05-17 15:36:38 +0800

    update jmx metric collector scripts

--- eagle-external/hadoop_jmx_collector/hadoop_ha_checker.py
+++ eagle-external/hadoop_jmx_collector/hadoop_ha_checker.py
@@ -22,38 +22,35 @@ import logging,socket,string
 class HadoopNNHAChecker(MetricCollector):
     def run(self):
         hosts = []
-        host_name_list = []
+
         for input in self.config["input"]:
             if not input.has_key("host"):
                 input["host"] = socket.getfqdn()
             if input.has_key("component") and input["component"] == "namenode":
                 hosts.append(input)
-                host_name_list.append(input["host"])
 
         if not bool(hosts):
             logging.warn("non hosts are configured as 'namenode' in 'input' config, exit")
             return
 
         logging.info("Checking namenode HA: " + str(hosts))
-        total_count = len(hosts)
-
-        all_hosts_name = string.join(host_name_list,",")
-
-        self.collect({
-            "host": all_hosts_name,
-            "component": "namenode",
-            "metric": "hadoop.namenode.hastate.total.count",
-            "value": total_count
-        })
 
         active_count = 0
         standby_count = 0
         failed_count = 0
 
+        failed_host_list = []
+        host_name_list = []
+
         for host in hosts:
             try:
-                bean = JmxReader(host["host"], host["port"], host["https"]).open().get_jmx_bean_by_name(
-                        "Hadoop:service=NameNode,name=FSNamesystem")
+                if host.has_key("source_host"):
+                    host["host"] = host["source_host"]
+
+                host_name_list.append(host["host"])
+                bean = JmxReader(host["host"], host["port"], host["https"]) \
+                    .read_query("/jmx?qry=Hadoop:service=NameNode,name=FSNamesystem&anonymous=true") \
+                    .get_jmx_bean_by_name("Hadoop:service=NameNode,name=FSNamesystem")
                 if not bean:
                     logging.error("JMX Bean[Hadoop:service=NameNode,name=FSNamesystem] is null from " + host["host"])
                 if bean.has_key("tag.HAState"):
@@ -67,6 +64,19 @@ class HadoopNNHAChecker(MetricCollector):
             except Exception as e:
                 logging.exception("failed to read jmx from " + host["host"] + ":" + host["port"])
                 failed_count += 1
+                failed_host_list.append(host["host"])
+
+
+        total_count = len(hosts)
+        all_hosts_name = string.join(host_name_list,",")
+
+        self.collect({
+            "host": all_hosts_name,
+            "component": "namenode",
+            "metric": "hadoop.namenode.hastate.total.count",
+            "value": total_count
+        })
+
         self.collect({
             "host": all_hosts_name,
             "component": "namenode",
@@ -81,12 +91,96 @@ class HadoopNNHAChecker(MetricCollector):
             "value": standby_count
         })
 
+        if len(failed_host_list) > 0:
+            all_hosts_name = string.join(failed_host_list,",")
+
         self.collect({
             "host": all_hosts_name,
             "component": "namenode",
             "metric": "hadoop.namenode.hastate.failed.count",
             "value": failed_count
         })
+
+class HadoopHBaseHAChecker(MetricCollector):
+    def run(self):
+        hosts = []
+
+        for input in self.config["input"]:
+            if not input.has_key("host"):
+                input["host"] = socket.getfqdn()
+            if input.has_key("component") and input["component"] == "hbasemaster":
+                hosts.append(input)
+
+        if not bool(hosts):
+            logging.warn("non hosts are configured as 'hbasemaster' in 'input' config, exit")
+            return
+
+        logging.info("Checking HBase HA: " + str(hosts))
+
+        active_count = 0
+        standby_count = 0
+        failed_count = 0
+
+        failed_host_list = []
+        host_name_list = []
+
+        for host in hosts:
+            try:
+                if host.has_key("source_host"):
+                    host["host"] = host["source_host"]
+                host_name_list.append(host["host"])
+                bean = JmxReader(host["host"], host["port"], host["https"]) \
+                    .read_query("/jmx?qry=Hadoop:service=HBase,name=Master,sub=Server&anonymous=true") \
+                    .get_jmx_bean_by_name("Hadoop:service=HBase,name=Master,sub=Server")
+                if not bean:
+                    logging.error("JMX Bean[Hadoop:service=HBase,name=Master,sub=Server] is null from " + host["host"])
+                if bean.has_key("tag.isActiveMaster"):
+                    logging.debug(str(host) + " is " + bean["tag.isActiveMaster"])
+                    if bean["tag.isActiveMaster"] == "true":
+                        active_count += 1
+                    else:
+                        standby_count += 1
+                else:
+                    logging.info("'tag.isActiveMaster' not found from jmx of " + host["host"] + ":" + host["port"])
+            except Exception as e:
+                logging.exception("failed to read jmx from " + host["host"] + ":" + host["port"])
+                failed_count += 1
+                failed_host_list.append(host["host"])
+
+        total_count = len(hosts)
+        all_hosts_name = string.join(host_name_list,",")
+
+        self.collect({
+            "host": all_hosts_name,
+            "component": "hbasemaster",
+            "metric": "hadoop.hbasemaster.hastate.total.count",
+            "value": total_count
+        })
+
+        self.collect({
+            "host": all_hosts_name,
+            "component": "hbasemaster",
+            "metric": "hadoop.hbasemaster.hastate.active.count",
+            "value": active_count
+        })
+
+        self.collect({
+            "host": all_hosts_name,
+            "component": "hbasemaster",
+            "metric": "hadoop.hbasemaster.hastate.standby.count",
+            "value": standby_count
+        })
+
+        if len(failed_host_list) > 0:
+            all_hosts_name = string.join(failed_host_list,",")
+
+        self.collect({
+            "host": all_hosts_name,
+            "component": "hbasemaster",
+            "metric": "hadoop.hbasemaster.hastate.failed.count",
+            "value": failed_count
+        })
+
 
 class HadoopRMHAChecker(MetricCollector):
     def run(self):
@@ -103,19 +197,12 @@ class HadoopRMHAChecker(MetricCollector):
             return
 
         logging.info("Checking resource manager HA: " + str(hosts))
-        total_count = len(hosts)
-        all_hosts_name = string.join(all_hosts,",")
-
-        self.collect({
-            "host": all_hosts_name,
-            "component": "resourcemanager",
-            "metric": "hadoop.resourcemanager.hastate.total.count",
-            "value": total_count
-        })
 
         active_count = 0
         standby_count = 0
         failed_count = 0
+
+        failed_host_list = []
 
         for host in hosts:
             try:
@@ -130,6 +217,17 @@ class HadoopRMHAChecker(MetricCollector):
             except Exception as e:
                 logging.error("Failed to read yarn ws from " + str(host))
                 failed_count += 1
+                failed_host_list.append(host["host"])
+
+        total_count = len(hosts)
+        all_hosts_name = string.join(all_hosts,",")
+
+        self.collect({
+            "host": all_hosts_name,
+            "component": "resourcemanager",
+            "metric": "hadoop.resourcemanager.hastate.total.count",
+            "value": total_count
+        })
 
         self.collect({
             "host": all_hosts_name,
@@ -145,6 +243,9 @@ class HadoopRMHAChecker(MetricCollector):
             "value": standby_count
         })
 
+        if len(failed_host_list) > 0:
+            all_hosts_name = string.join(failed_host_list,",")
+
         self.collect({
             "host": all_hosts_name,
             "component": "resourcemanager",
@@ -153,4 +254,4 @@ class HadoopRMHAChecker(MetricCollector):
         })
 
 if __name__ == '__main__':
-    Runner.run(HadoopNNHAChecker(), HadoopRMHAChecker())
+    Runner.run(HadoopNNHAChecker(), HadoopHBaseHAChecker(), HadoopRMHAChecker())
