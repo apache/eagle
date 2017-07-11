0f577646ee712078558e01d910adff77c4618861
zombieJ <smith3816@gmail.com>
2017-07-11 14:27:01 +0800

    fix logic of wrap entity

--- eagle-server/src/main/webapp/app/dev/public/js/services/entitySrv.js
+++ eagle-server/src/main/webapp/app/dev/public/js/services/entitySrv.js
@@ -34,7 +34,11 @@
 			list._promise = promise.then(function (res) {
 				var data = res.data;
 				list.splice(0);
-				Array.prototype.push.apply(list, data.data);
+				if (typeof data.data === 'object') {
+					Array.prototype.push.apply(list, data.data);
+				} else {
+					list.push(data.data);
+				}
 				list._done = true;
 
 				return res;
